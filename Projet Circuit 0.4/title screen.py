9fadc245065b41a2c0072a673035c790074798ff
OmegaPhi115 <45884990+OmegaPhi115@users.noreply.github.com>
2018-12-14 22:10:46 +0100

    test2

---
+++ Projet Circuit 0.4/title screen.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        title screen
+# Purpose:
+#
+# Author:      François
+#
+# Created:     14/12/2018
+# Copyright:   (c) François 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from tkinter import *
+from math import *
+import random
+
+fen = Tk()
