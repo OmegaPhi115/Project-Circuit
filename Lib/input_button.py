d54a28dd6234ecb3dcd8d3221df195d5c7c56c49
OmegaPhi115 <45884990+OmegaPhi115@users.noreply.github.com>
2019-01-10 21:15:23 +0100

    Update input_button.py

---
+++ Lib/input_button.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+class input_button():
+    """boutton D'entrée"""
+    #1) graphismes
+    self.imgon = PhotoImage(file="images\\OR.gif")
+    self.imgoff = PhotoImage(file="images\\OR.gif")
