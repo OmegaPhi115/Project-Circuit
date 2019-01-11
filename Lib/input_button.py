5206b402bcf21911fcec9520718947a256ac9052
OmegaPhi115 <45884990+OmegaPhi115@users.noreply.github.com>
2019-01-11 17:35:30 +0100

    t

--- Lib/input_button.py
+++ Lib/input_button.py
@@ -3,5 +3,12 @@ import tkinter as tk
 class input_button():
     """boutton D'entrée"""
     #1) graphismes
-    self.imgon = PhotoImage(file="images\\OR.gif")
-    self.imgoff = PhotoImage(file="images\\OR.gif")
+    self.imgon = tk.PhotoImage(file="images\\button_off.gif")
+    self.imgoff = tk.PhotoImage(file="images\\button_on.gif")
+
+    #position
+    self.xPos = x
+    self.yPos = y
+
+    #logique
+    self.state = "on" #off
