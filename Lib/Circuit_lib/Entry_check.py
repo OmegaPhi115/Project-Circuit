b59601993b1577b5ae1af86afdc86a375fa7bea1
François Pinguenet <omegaphi.mail@gmail.com>
2019-01-24 02:06:53 +0100

    Fix gros bug

--- Lib/Circuit_lib/Entry_check.py
+++ Lib/Circuit_lib/Entry_check.py
@@ -1,15 +1,22 @@
 class Main():
 	"""Regarde si les entrées sont valides sinon erreur: OperationError"""
-	class OperationError():
+	class OperationError(Exception):
 		"""Erreur D'entrée d'operation logique"""
 		def __init__(self, wrong_operation):
 			print("Erreur D'entrée d'operation logique")
 			print(str(wrong_operation) + " n'est pas une entrée correcte")
+	
+	class ValueError(Exception):
+		"""Erreur D'entrée d'operation logique"""
+		def __init__(self, wrong_operation):
+			print("Erreur D'entrée d'operation logique")
+			print(str(wrong_operation) + " n'est pas une entrée correcte, doit etre True ou False")
 
-	def operation(op):
+	def operation(self, op):
 		if op != "or" or "and" or "xor" or "nor" or "nand" or "nxor":
-			raise OperationError
+			raise self.OperationError(op)
 
-	def entry(a):
+	def entry(self, a):
 		#entrées
-		pass
+		if a != True or False:
+			raise self.ValueError(a)
