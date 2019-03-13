7131706e423cc517d33e9606a22876666db5e180
François Pinguenet <omegaphi.mail@gmail.com>
2019-03-13 21:41:53 +0100

    VS code pls marche !

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+#imports
 import pygame
 
+#creation surface
 pygame.init()
-window_resolution = (640,480)
+window_resolution = (640,480) #resolution de la surface en pixls (tupple)
+window_game = pygame.display.set_mode(window_resolution)
+
+
+
+
+#mainloop
