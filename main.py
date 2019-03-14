930c934ee26992633e959918b03ac652248ba36e
François Pinguenet <omegaphi.mail@gmail.com>
2019-03-14 01:20:37 +0100

    Pycharm

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import pygame
 
 #creation surface
 pygame.init()
-window_resolution = (640,480) #resolution de la surface en pixls (tupple)
+window_resolution = (640,480)#resolution de la surface en pixls (tupple)
 window_game = pygame.display.set_mode(window_resolution)
 
 
 
 
 #mainloop
+Launched = True
+while Launched == True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Launched = False
